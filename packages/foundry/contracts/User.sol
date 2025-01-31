9b0381571ede35c0304d90d55c2f8fedec8f92f4
dipanshuhappy <dipanshuhappy@gmail.com>
2025-01-31 17:58:41 +0530

    Add empty createBond method with modified signature in User contract

--- packages/foundry/contracts/User.sol
+++ packages/foundry/contracts/User.sol
@@ -13,75 +13,79 @@ import "./interfaces/IFeeSettings.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract User is IUser {
-    mapping(address => IBond.BondDetails) private bondDetails;
-    mapping(string => bool) private verifiedIdentities;
+  mapping(address => IBond.BondDetails) private bondDetails;
+  mapping(string => bool) private verifiedIdentities;
 
-    UserDetails public user;
- 
+  UserDetails public user;
 
-    IIdentityRegistry private identityRegistry;
-    IFeeSettings private feeSettings;
+  IIdentityRegistry private identityRegistry;
+  IFeeSettings private feeSettings;
 
-    mapping (string=>string) slashingWords;
+  mapping(string => string) slashingWords;
 
+  constructor(address _identityRegistry, address _userWalletSettings) {
+    identityRegistry = IIdentityRegistry(_identityRegistry);
+    feeSettings = IFeeSettings(_userWalletSettings);
+    user = UserDetails({
+      userAddress: msg.sender,
+      totalBonds: 0,
+      totalAmount: 0,
+      totalWithdrawnBonds: 0,
+      totalBrokenBonds: 0,
+      totalActiveBonds: 0,
+      totalWithdrawnAmount: 0,
+      totalBrokenAmount: 0,
+      createdAt: block.timestamp
+    });
 
-    constructor(address _identityRegistry,address _userWalletSettings) {
-        identityRegistry = IIdentityRegistry(_identityRegistry);
-        feeSettings = IFeeSettings(_userWalletSettings);
-        user = UserDetails({
-            userAddress: msg.sender,
-            totalBonds: 0,
-            totalAmount: 0,
-            totalWithdrawnBonds: 0,
-            totalBrokenBonds: 0,
-            totalActiveBonds: 0,
-            totalWithdrawnAmount: 0,
-            totalBrokenAmount: 0,
-            createdAt: block.timestamp
-        });
-        
-        emit UserCreated(msg.sender, block.timestamp);
-    }
+    emit UserCreated(msg.sender, block.timestamp);
+  }
 
-    /*
+  /*
     ----------------------------------
     ------EXTERNAL OPEN FUNCTIONS-----
     ----------------------------------
     */
 
-    function createBond(
-        IBond.BondDetails memory _bond,
-        address _aavePoolAddress,
-        address _uiPoolDataAddress,
-        address _ypsFactoryAddress,
-        address _bondFactoryAddress
-    ) external returns (bool) {
-        IBondFactory bondFactory = IBondFactory(_bondFactoryAddress);
-        address newBond = bondFactory.createBond(
-            _bond.asset,
-            _bond.user1,
-            _bond.user2,
-            _bond.totalBondAmount,
-            _aavePoolAddress,
-            _uiPoolDataAddress,
-            _ypsFactoryAddress
-        );
+  function createBond(
+    IBond.BondDetails memory _bond,
+    address _aavePoolAddress,
+    address _uiPoolDataAddress,
+    address _ypsFactoryAddress,
+    address _bondFactoryAddress
+  ) external returns (bool) {
+    IBondFactory bondFactory = IBondFactory(_bondFactoryAddress);
+    address newBond = bondFactory.createBond(
+      _bond.asset,
+      _bond.user1,
+      _bond.user2,
+      _bond.totalBondAmount,
+      _aavePoolAddress,
+      _uiPoolDataAddress,
+      _ypsFactoryAddress
+    );
 
-        bondDetails[newBond] = _bond;
-        emit BondDeployed(_bond.asset, _bond.user1, _bond.user2, _bond.totalBondAmount, block.timestamp);
-        return true;
-    }
+    bondDetails[newBond] = _bond;
+    emit BondDeployed(_bond.asset, _bond.user1, _bond.user2, _bond.totalBondAmount, block.timestamp);
+    return true;
+  }
 
-    function getBondDetails(address _bondAddress) external view returns (IBond.BondDetails memory) {
-        return bondDetails[_bondAddress];
-    }
+  function getBondDetails(address _bondAddress) external view returns (IBond.BondDetails memory) {
+    return bondDetails[_bondAddress];
+  }
 
-    function verifyIdentity(string calldata identityTag, bytes calldata data) external returns (bool) {
-        address resolver = identityRegistry.getResolver(identityTag);
-        require(resolver != address(0), "Resolver not found");
+  function verifyIdentity(string calldata identityTag, bytes calldata data) external returns (bool) {
+    address resolver = identityRegistry.getResolver(identityTag);
+    require(resolver != address(0), "Resolver not found");
+    bool verified = IIdentityResolver(resolver).verify(data);
+    verifiedIdentities[identityTag] = verified;
+    return verified;
+  }
 
-        bool verified = IIdentityResolver(resolver).verify(data);
-        verifiedIdentities[identityTag] = verified;
-        return verified;
-    }
+  function createBond(
+    IBond.BondDetails memory _bond,
+    address _aavePoolAddress,
+    address _uiPoolDataAddress,
+    address _ypsFactoryAddress
+  ) external override returns (bool) {}
 }

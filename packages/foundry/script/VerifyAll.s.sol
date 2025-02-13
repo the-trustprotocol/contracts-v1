ba37723453dc13406032e422d1cf8a4af9ffe55a
Nithin-Varma <mnithin1422@gmail.com>
2025-02-13 18:42:34 +0530

    sepolia deployment

--- packages/foundry/script/VerifyAll.s.sol
+++ packages/foundry/script/VerifyAll.s.sol
@@ -26,7 +26,7 @@ contract VerifyAll is Script {
     function run() external {
         string memory root = vm.projectRoot();
         string memory path =
-            string.concat(root, "/broadcast/DeployV1_2Sepolia.s.sol/", vm.toString(block.chainid), "/run-latest.json");
+            string.concat(root, "/broadcast/DeployV1_3Sepolia.s.sol/", vm.toString(block.chainid), "/run-latest.json");
         string memory content = vm.readFile(path);
 
         while (this.nextTransaction(content)) {
